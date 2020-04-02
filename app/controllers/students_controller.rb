8c8f4ffbcf92c5cf2e2bf7fe165f15180b5e25ec
Vivica <Ma>
2020-04-01 23:46:34 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,15 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: :show
   def index
     @students = Student.all
   end
+  
+  private
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
+  # def show
+  #   @student = Student.find(pramas[:id])
+  # end 
 end
